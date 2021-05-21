4075befcea9df05cfd73c83428f89f1bda48d65f
pmapelli <pmapelli@hotmail.com>
2021-05-21 11:27:24 -0300

    feat: Controller de Autenticação

---
+++ src/DevIo.Api/Controllers/AuthController.cs
@@ -0,0 +1,72 @@
+using System.Threading.Tasks;
+using DevIo.Api.Dtos;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevIo.Api.Controllers
+{
+    [Route("api")]
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotificador notificador, SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager) : base(notificador)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(RegisterUserDto registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(result); 
+            }
+
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("entrar")]
+        public async Task<ActionResult> Login(LoginUserDto loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse(loginUser);
+            }
+
+            if (result.IsLockedOut)
+            {
+                NotificarErro("Usuário temporariamente bloqueado por tentativas inválidas");
+            }
+            
+            NotificarErro("Usuário ou Senha incorretos");
+
+            return CustomResponse(loginUser);
+        }
+    }
+}
--- src/DevIo.Api/Data/ApplicationDbContext.cs
+++ src/DevIo.Api/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevIo.Api.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
